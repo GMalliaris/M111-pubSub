0d55dd5bfde79833be3db80fbfb8d051f819c950
GMalliaris <georgemalliaris8@gmail.com>
2022-05-29 16:40:57 +0300

    added impl for subs

--- src/Broker.java
+++ src/Broker.java
@@ -1,15 +1,17 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 //broker -s s_port -p p_port
 public class Broker {
 
-    private static Map<String, Set<String>> topicSubscribers = new HashMap<>();
-    private static Map<String, Socket> subscriberSockets = new HashMap<>();
+    private static final Map<String, Set<String>> topicSubscribers = new HashMap<>();
+    private static final Map<String, Socket> subscriberSockets = new HashMap<>();
     private static int subPort;
     private static int pubPort;
 
@@ -27,11 +29,14 @@ public class Broker {
         try (var pubServerSocket = new ServerSocket(pubPort);
             var subServerSocket = new ServerSocket(subPort)) {
 //            1 socket for pubServerSocket.accept and 1 socket for each pub
-            while (true) {
-                var pubSocket = pubServerSocket.accept();
-
-                readPubCommandAndReply(pubSocket);
-            }
+//            while (true) {
+//                var pubSocket = pubServerSocket.accept();
+//                readPubCommandAndReply(pubSocket);
+//            }
+//            while (true) {
+//                var subSocket = subServerSocket.accept();
+//                readSubCommandAndReply(subSocket);
+//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -99,20 +104,87 @@ public class Broker {
         System.out.println(message);
     }
 
-    private static void readPubCommandAndReply(Socket clientSocket){
-        try (var clientOutStream = new PrintWriter(clientSocket.getOutputStream(), true);
-             var clientInStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))  ){
+    private static void readPubCommandAndReply(Socket pubSocket){
+        try (var pubOutStream = new PrintWriter(pubSocket.getOutputStream(), true);
+             var pubInStream = new BufferedReader(new InputStreamReader(pubSocket.getInputStream()))  ){
 
-            var inputLine = clientInStream.readLine();
+            var inputLine = pubInStream.readLine();
             while (inputLine != null) {
                 Thread.sleep(1000);
                 // send msg to subs
-                clientOutStream.println("OK");
-                inputLine = clientInStream.readLine();
+                var split = inputLine.split(" ", 4);
+                sendMessageToTopic(split[3], split[2]);
+                pubOutStream.println("OK");
+                inputLine = pubInStream.readLine();
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             Thread.currentThread().interrupt();
         }
+    }
+
+    private static void readSubCommandAndReply(Socket subSocket){
+        try (var subOutStream = new PrintWriter(subSocket.getOutputStream(), true);
+             var subInStream = new BufferedReader(new InputStreamReader(subSocket.getInputStream()))  ){
+
+            var inputLine = subInStream.readLine();
+            while (inputLine != null) {
+                Thread.sleep(1000);
+                var split = inputLine.split(" ", 3);
+                synchronized (subscriberSockets){
+                    if (subscriberSockets.get(split[0]) == null) {
+                        subscriberSockets.put(split[0], subSocket);
+                    }
+                }
+                if ("sub".equals(split[1])){
+                    subscribeToTopic(split[0], split[2]);
+                }
+                else {
+                    unsubscribeFromTopic(split[0], split[2]);
+                }
+                subOutStream.println("OK");
+                inputLine = subInStream.readLine();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static void subscribeToTopic(String subId, String topic) {
+        synchronized (topicSubscribers){
+            var topicSubscribers = Broker.topicSubscribers.getOrDefault(topic, new HashSet<>());
+            topicSubscribers.add(subId);
+            Broker.topicSubscribers.put(topic, topicSubscribers);
+        }
+    }
+
+    private static void unsubscribeFromTopic(String subId, String topic) {
+        synchronized (topicSubscribers){
+            var topicSubscribers = Broker.topicSubscribers.get(topic);
+            topicSubscribers.remove(subId);
+            Broker.topicSubscribers.put(topic, topicSubscribers);
+        }
+    }
+
+    private static void sendMessageToTopic(String message, String topic){
+
+        List<Socket> subsSockets;
+        synchronized (topicSubscribers){
+            synchronized (subscriberSockets){
+                subsSockets = topicSubscribers.getOrDefault(topic, Set.of())
+                        .stream()
+                        .map(subscriberSockets::get)
+                        .collect(Collectors.toList());
+            }
+        }
+        subsSockets.forEach(subSocket -> {
+            try (var clientOutStream = new PrintWriter(subSocket.getOutputStream(), true)){
+                clientOutStream.println(String.format("%s %s", topic, message));
+            } catch (IOException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        });
     }
 }
